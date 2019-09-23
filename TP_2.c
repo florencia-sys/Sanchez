1230a86cf0e10211531541296317eafab7040b3a
florencia-sys <52495075+florencia-sys@users.noreply.github.com>
2019-09-23 12:01:49 -0300

    Add files via upload

---
+++ TP_2.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "TP_2.h"
+
+int initEmployees (arrayEmployees[],ELEMENTS);
+{
+    if ( arrayEmployees[i] == -1)
+    {
+        printf ("No quedan lugares");
+    }
+    else{
+          for (int i = 0; i < 1000; i ++)
+    {
+    printf ("Ingrese nombre de empleado: \n");
+    gets (name);
+    printf ("Ingrese apellido de empleado: \n");
+    gets (lastName);  for (int i = 0; i < 1000; i ++)
+    {
+    printf ("Ingrese salario de empleado: \n");
+    scanf ("%d", Employee[i].salary);
+    printf ("Ingrese sector de empleado: \n");
+    scanf ("%d", Employee[i].sector);
+    }
+         }
+
+    int addEmployees (Employee[list], int len, int id, char name[],char lastName[],float salary,int sector)
+    {
+        printf ("Ingrese nombre de empleado nuevo: "\n);
+        gets (name);
+    }
+    
+
+
+
+
+}
